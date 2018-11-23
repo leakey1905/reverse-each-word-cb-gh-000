13eea15b13b24bb84f9ea6251b9f3aa484a5ad8c
Ji Li <liji15@mails.tsinghua.edu.cn>
2018-11-23 05:40:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(sentence)
   sentence_to_array = sentence.split
-  sentence_to_array.collect{|word| word.reverse!}
-  sentence_to_array.join(" ")
+  sentence_to_array.collect{|word| word.reverse!}.join(" ")
 end
